612e64a4c2b92f43b664d1fa99135682cae21090
bedinhcao <pham.huy@stud.h-da.de>
2020-09-25 23:30:59 +0200

    Display Movie List

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Aux from '../../hoc/UAX'
+import "./Movie.css"
 
 const movie = (props) => {
+    let imgurl = "http://image.tmdb.org/t/p/w185/" + props.poster
+    console.log(imgurl)
     return(
-        <Aux>
-            <img/>
-            <h4>Movies Title</h4>
+        <Aux >
+            <div className = "Movie">
+                <img src = {imgurl}/>
+                <h4>{props.title}</h4>
+                <p>{props.overview}</p>
+            </div>
         </Aux>
     )
 }
--- src/Containers/MoviesContainers.js
+++ src/Containers/MoviesContainers.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import Aux from '../hoc/UAX'
 import Movie from '..//Components/Movie/Movie'
-
+import axios from 'axios'
+import "./MoviesContainers.css"
+ 
 class Moviescontainers extends Component {
+    state = {
+        series : []
+    }
+    componentDidMount () {
+        axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=2e3c4a94c1657ecbda2df2936291e602&language=en-US&page=1').
+        then(response => {
+            const series = response.data.results
+            const Updatedseries = series.map(post =>{
+                return {
+                    ...post,
+                }
+            })
+            this.setState({series : Updatedseries})
+            console.log(Updatedseries)
+            console.log(response)
+        }
+        )
+    }
+    
     render(){
+        const movieposts = this.state.series.map(post => {
+            return <Movie title = {post.original_title} key = {post.id} overview = {post.overview} poster = {post.poster_path}/>
+        }) 
         return(
         <Aux>
-        <Movie/>
+        <div className = "MoviesContainers">
+        {movieposts}
+        </div>
         </Aux>
-            
         )
     }
 }
